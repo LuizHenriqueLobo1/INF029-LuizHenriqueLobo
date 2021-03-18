2dca2cc69b032fb7e48c1e82f3128ec65b4c5cc5
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2021-03-18 15:43:03 -0300

    Adicionando professor.c

--- Projeto_Escola/Projeto_Escola_v1/projeto_escola_adendos.h
+++ Projeto_Escola/Projeto_Escola_v1/projeto_escola_adendos.h
@@ -8,13 +8,28 @@ typedef struct {
 	char cpf[50];
 }dados;
 
+// Menus
 int menu_principal();
 int menuAlunos();
+int menuProfessor();
+int menu_alterarCadastroA(int num);
+int menu_alterarCadastroP(int num);
+
+// Aluno
 int mainAlunos(dados estud[], int quantidadeAlunos);
 void cadastrarAlunos(int qtdAlunos, dados estudante[]);
 void mostrarAlunos(int qtdAlunos, dados estudante[]);
 void alterarCadastroAlunos(int qtdAlunos, dados estudante[]);
 int removerAluno(int qtdAlunos, dados estudante[]);
+
+// Professor
+int mainProfessor(dados prof[], int quantidadeProfessores);
+void cadastrarProfessor(int qtdProf, dados professor[]);
+void mostrarProfessor(int qtdProf, dados professor[]);
+void alterarCadastroProfessor(int qtdProf, dados professor[]);
+int removerProfessor(int qtdProf, dados professor[]);
+
+// Validações
 int validarMatricula(char matricula[]);
 int validarNome(char nome[]);
 int validarSexo(char sexo);
