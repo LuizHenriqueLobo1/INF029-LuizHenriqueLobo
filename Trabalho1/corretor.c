715fb8264b2942498ffc2610c480a89f7c0215cc
LuizHenriqueLobo1 <luizhenriquelobo14@gmail.com>
2021-04-23 15:48:01 -0300

    Adicionando q4 completa

--- Trabalho1/corretor.c
+++ Trabalho1/corretor.c
@@ -31,9 +31,9 @@ int main(){
     //testSomar();
     //testFatorial();
     //testQ1();
-    testQ2();
+    //testQ2();
     //testQ3();
-    //testQ4();
+    testQ4();
     //testQ5();
     //testQ6();
 }
