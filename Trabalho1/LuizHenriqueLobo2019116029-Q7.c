ae010a43c85aa1cd5bdb67250f098420b28e07b5
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2021-04-29 22:19:13 -0300

    Atualizando Q7 (Jogo da Velha)

--- Trabalho1/LuizHenriqueLobo2019116029-Q7.c
+++ Trabalho1/LuizHenriqueLobo2019116029-Q7.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int menu();
 void preencherMatriz(int matriz[3][3]);
 void printarMatriz(int matriz[3][3]);
 void jogar(int matriz[3][3]);
+int validarCelula(char celula[10]);
 
 int main() {
 
@@ -21,7 +24,7 @@ int main() {
 		switch(opcao) {
 
 			case 1: {
-				// Ainda em implementação...
+				jogar(matriz);
 				break;
 			}
 
@@ -76,4 +79,64 @@ void printarMatriz(int matriz[3][3]) {
 		}
 	}
 	printf("\n\n");
+}
+
+void jogar(int matriz[3][3]) {
+
+	int loop = 1;
+	char celula[10];
+	char linha, coluna;
+	int iLinha, iColuna;
+
+	while(loop == 1) {
+
+		setbuf(stdin, NULL);
+		printf("\nDICA -> (A, B, C = Linhas / 1, 2, 3 = Colunas)\n");
+		printf("DIGITE A CELULA: ");
+		fgets(celula, 10, stdin);
+		while(1) {
+			if(validarCelula(celula) != 1) {
+				printf("\nCELULA INVALIDA! Digite novamente...\n");
+				fgets(celula, 10, stdin);
+			} else {
+				break;
+			}
+		}
+
+		linha = celula[0];
+		linha = toupper(linha);
+		coluna = celula[1];
+		
+		printf("\nLinha: %c", linha);
+		printf("\nColuna: %c\n", coluna);
+
+		loop = 0;
+	}
+
+}
+
+int validarCelula(char celula[10]) {
+
+	int retorno;
+	int tamCelula;
+	int i;
+
+	tamCelula = strlen(celula);
+	tamCelula--;
+
+	// Validação do tamanho
+	if(tamCelula != 2)
+		retorno = -1;
+	else
+		retorno = 1;
+
+	// Validação dos caracteres
+	if((toupper(celula[0] == 'A')) || (toupper(celula[0] == 'B')) || (toupper(celula[0] == 'C')))
+		retorno = 1;
+	if((celula[1] == '1') || (celula[1] == '2') || (celula[1] == '3'))
+		retorno = 1;
+	else
+		retorno = -1;
+	
+	return retorno;
 }
