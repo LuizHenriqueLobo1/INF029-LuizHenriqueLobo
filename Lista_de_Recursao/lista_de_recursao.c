ceaa4f7828f7daedfb54bfd770c8e69f4471393e
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2021-06-03 15:39:09 -0300

    Atualizando organização do escopo

--- Lista_de_Recursao/lista_de_recursao.c
+++ Lista_de_Recursao/lista_de_recursao.c
@@ -3,39 +3,40 @@
 #include "lista_de_recursao_headers.h"
 
 void main() {
-	// Questão 1
-	int n = 6;
-	n = fatorial(n);
-	printf("\n----------Questao 1----------\n");
-	printf("Fatorial: %d", n);
-	printf("\n-----------------------------\n");
+	questao1();
+	questao2();
+}
 
-	// Questão 2
+void separadorInicio(char string[]) {
+	printf("\n---------- %s ----------\n", string);
+}
+
+void separadorFinal() {
+	printf("\n-------------------------------\n");
+}
+
+void questao1() {
+
+	int n = 6;
+	
+	n = fatorial(n);
+	
+	separadorInicio("Questao 1");
+	printf("Fatorial: %d", n);
+	separadorFinal();
+}
+
+void questao2() {
+
 	int tamSeq = 10;
 	int r = 0;
-	printf("\n----------Questao 2----------\n");
+	int i;
+
+	separadorInicio("Questao 2");
 	printf("Sequencia Fibonacci:\n");
-	for(int i = 0; i <= tamSeq; i++) {
+	for(i = 0; i <= tamSeq; i++) {
     	r = fibonacci(i);
     	printf("%d ", r);       
 	}
-	printf("\n-----------------------------\n");
-}
-
-// Questão 1
-int fatorial(int n) {
-	if(n <= 0)
-		return 1;
-	else
-		return n * fatorial(n - 1) * -1;
-}
-
-// Questão 2
-int fibonacci(int n) {
-	if(n == 0)
-		return 0;
-	else if(n < 3)
-		return 1;
-	else 
-		return fibonacci(n - 1) + fibonacci(n - 2);
+	separadorFinal();
 }
