0f0489efcff235679117a5ef55e642a56eac3a07
LuizHenriqueLobo1 <luizhenriquelobo14@gmail.com>
2021-06-03 15:39:09 -0300

    Atualizando organização do escopo

--- Lista_de_Recursao/lista_de_recursao_headers.h
+++ Lista_de_Recursao/lista_de_recursao_headers.h
@@ -1,2 +1,11 @@
+// Resoluções das questões
 int fatorial(int n);
 int fibonacci(int n);
+
+// Chamadas das questões
+void questao1();
+void questao2();
+
+// Outros
+void separadorInicio(char string[]);
+void separadorFinal();
---
+++ Lista_de_Recursao/resolucao_questoes.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include "lista_de_recursao_headers.h"
+
+// Q1
+int fatorial(int n) {
+	if(n <= 0)
+		return 1;
+	else
+		return n * fatorial(n - 1) * -1;
+}
+
+// Q2
+int fibonacci(int n) {
+	if(n == 0)
+		return 0;
+	else if(n < 3)
+		return 1;
+	else 
+		return fibonacci(n - 1) + fibonacci(n - 2);
+}
